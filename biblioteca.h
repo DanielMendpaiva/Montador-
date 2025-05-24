9ca115a3406ce4d9921b3f496a4b436622e710b2
D4N13Lm <paivadaniel246@gmail.com>
2025-05-24 14:25:31 -0300

    Adiciona definições de funções em biblioteca.h e implementações iniciais

---
+++ biblioteca.h
@@ -0,0 +1,9 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+//definições de funções 
+//EX: int_fatorail(int);
+//definições de váriaveis globais  
+
+
+#endif 
---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+#include "biblioteca.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	//programação 
+	
+	
+	return 0;
+}
